81e7712525929e3348e18464e120f1cb3e1d84c4
wiz-codez <134593442+wiz-codez@users.noreply.github.com>
2023-05-25 13:11:28 +0100

    DONE

--- assignment.py
+++ assignment.py
@@ -12,3 +12,18 @@ else:
             print('Valid password')
             break
         is_running=False
+
+login_username = input("what is your name: ")
+login_password = input("Enter your password: ")
+login_confirm_password = input("right password: ")
+is_running = True
+if login_password == confirm_password and login_confirm_password == login_password:
+    print('Valid password')
+else:
+    while is_running:
+        print("Wrong password ")
+        login_confirm_password = input("right password ")
+        if login_password == confirm_password and login_confirm_password == login_password:
+            print('Valid password')
+            break
+        is_running=False
